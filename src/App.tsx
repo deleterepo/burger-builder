c253a36ab13a462a261944797ff393c0c87717cf
Jordan Sherman <jordandsherman@gmail.com>
2018-11-17 18:49:58 -0500

    Added Layout and Auxiliary components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
+import Layout from './components/Layout/Layout';
 
 class App extends React.Component {
   public render() {
     return (
       <div>
-        Burger Builder
+        <Layout>
+          <p>Burger builder in the haus</p>
+        </Layout>
       </div>
     );
   }
