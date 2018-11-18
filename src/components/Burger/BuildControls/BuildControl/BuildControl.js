5f91e599a83d3250e31af5cbb7a4136cc943e561
Jordan Sherman <jordandsherman@gmail.com>
2018-11-18 16:45:33 -0500

    Added jsx for BuildControls and BuildControl components.

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -2,8 +2,8 @@ import React from 'react';
 import classes from './BuildControl.module.css';
 
 const buildControl = (props) => (
-    <div>
-        <div className={classes.BuildControl}>{props.label}</div>
+    <div className={classes.BuildControl}>
+        <div className={classes.Label}>{props.label}</div>
         <button className={classes.Less}>Less</button>
         <button className={classes.More}>More</button>
     </div>
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const buildControls = (props) => {
-    <div>
-        
+import BuildControl from './BuildControl/BuildControl';
+import classes from './BuildControls.module.css';
+
+const controls = [
+    { label: 'Meat', type: 'meat' },
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+];
+
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+        {controls.map(ctrl => (
+            <BuildControl key={ctrl.label} label={ctrl.label} />
+        ))}
     </div>
-}
+)
 
 export default buildControls;
